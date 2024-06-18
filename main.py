588c8beab683cfb0d7873acd9978a7b316ca4da9
bmanquen <bmanquen1993@gmail.com>
2024-06-18 17:10:09 -0500

    Added formatted report

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 def main():
+    file = "./books/frankenstein.txt"
     file_contents = ""
-    with open("./books/frankenstein.txt") as f:
+    with open(file) as f:
         file_contents = f.read()
-    print(f"{count_words(file_contents)} words found in the file")
+    print(f"--- Begin report of {file} ---")
+    print(f"{count_words(file_contents)} words found in the file\n")
+    chars = count_characters(file_contents)
+
+    chars = dict(sorted(chars.items(), key=lambda x:x[1], reverse=True))
+    for key in chars:
+        if key.isalpha():
+            print(f"The '{key}' character was found {chars[key]} times")
+    print("--- End report ---")
 
 def count_words(file_contents):
     words = file_contents.split()
     return len(words)
+
+def count_characters(file_contents):
+    characters = {}
+    for char in file_contents:
+        if char.lower() not in characters:
+            characters[char.lower()] = 1
+        else:
+            characters[char.lower()] += 1
+    return characters
 
 main()
